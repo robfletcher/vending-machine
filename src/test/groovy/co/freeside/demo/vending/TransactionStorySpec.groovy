b3f319758c28cf1ab50be0c64664d742446fcbd3
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-07 14:17:38 +0200

    catch-all spec for discussing specialised features

---
+++ src/test/groovy/co/freeside/demo/vending/TransactionStorySpec.groovy
@@ -0,0 +1,66 @@
+package co.freeside.demo.vending
+
+import spock.lang.*
+
+import static co.freeside.demo.vending.Coin.Quarter
+import static co.freeside.demo.vending.Product.Slurm
+import static co.freeside.demo.vending.Product.ApolloBar
+import static co.freeside.demo.vending.Coin.Dime
+
+@Stepwise
+class TransactionStorySpec extends Specification {
+
+	@Shared VendingMachine machine = new VendingMachine()
+	HardwareDevice hardware = Mock(HardwareDevice)
+
+	void setupSpec() {
+		// the machine is well stocked...
+		for (product in Product.values()) machine.addStock(10, product)
+
+		// ... and has plenty of change
+		for (coin in Coin.values()) machine.loadChange(20, coin)
+	}
+
+	void setup() {
+		machine.hardware = hardware
+	}
+
+	void 'user can accumulate credit'() {
+		when:
+		7.times {
+			machine.insertCoin(Quarter)
+		}
+
+		then:
+		machine.readCredit() == 7 * Quarter.value
+	}
+
+	void 'user can buy a product'() {
+		when:
+		machine.purchase(product)
+
+		then:
+		1 * hardware.dispense(product)
+
+		and:
+		machine.readCredit() == old(machine.readCredit()) - product.price
+
+		where:
+		product << [Slurm, ApolloBar]
+	}
+
+	void 'user can get change'() {
+		when:
+		machine.returnCoins()
+
+		then:
+		1 * hardware.returnCoin(Quarter)
+		1 * hardware.returnCoin(Dime)
+	}
+
+	void 'credit is deducted'() {
+		expect:
+		machine.readCredit() == 0
+	}
+
+}
